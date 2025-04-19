131d1eb5b5aba6728e512fb18745738daa63ea55
Vinicius Barbosa Soares da Silva <viniciusdl.soares@gmail.com>
2025-04-19 00:33:45 -0300

    commit: alteraçoes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 }) 
